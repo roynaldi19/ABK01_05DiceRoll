748068104878a49fa2a3953562df535fe86a8331
roynaldi19 <roynaldi19@gmail.com>
2021-06-22 23:26:15 +0700

    comit 1

--- app/src/main/java/com/example/abk01_05diceroll/MainActivity.kt
+++ app/src/main/java/com/example/abk01_05diceroll/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.abk01_05diceroll
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.abk01_05diceroll.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.button.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
